6569cb9382043a946e96134c123f1e512bb55b5e
Julio Gonzalez <julio.gteran@gmail.com>
2016-09-27 16:59:10 +0100

    unfinished

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,9 @@
+require 'bike.rb'
 class DockingStation
 
 	def release_bike
+		bike = Bike.new
+		#bike.working? == true
 	end
 
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,22 @@
 require 'docking_station.rb'
+require 'bike.rb'
 
 describe DockingStation do
 
 	it { is_expected.to respond_to :release_bike }
-	
+
 # Long way
 # it "respond to the method release bike" do
 #   expect(subject).to respond_to :release_bike
 # end
+
+
+it "gets a bike and checks if it works" do
+  docking_station = object_double(DockingStation.new)
+  bike = object_double(Bike.new)
+  docking_station.stub(:release_bike) {bike}
+  #expect(subject.release_bike).to eq bike
+  #expect(bike.working?).to eq true
+end
 
 end

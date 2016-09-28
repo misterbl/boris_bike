afaf05cd9b2d40e56ef4da6f9ae3400b02227005
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-28 13:57:55 +0100

    Docking Station releases a bike and ensures that it is working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
-    it {is_expected.to respond_to :release_bike}
+
+  it {is_expected.to respond_to :release_bike}
+
+  it 'gets a working bike' do
+
+  	bike = subject.release_bike
+		expect(bike).to be_an_instance_of Bike
+		expect(bike).to be_working 
+
+	end
+
 end

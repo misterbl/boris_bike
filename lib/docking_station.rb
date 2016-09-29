6552869bc748a05604d5af7bee48d0e1dff776d5
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-09-29 11:41:37 +0100

    add error test for release_bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,15 @@
+require_relative 'bike'
 class DockingStation
 
   attr_reader :bike
 
   def release_bike
-    raise 'No bike'
-    Bike.new
+  	raise 'No bikes available' unless @bike
+    @bike
   end
 
   def dock(bike)
-  	  @bike = bike
+  	@bike = bike 
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,24 +4,28 @@ describe DockingStation do
 
   it {is_expected.to respond_to :release_bike}
 
-  it 'gets a working bike' do
-
-  	bike = subject.release_bike
-		expect(bike).to be_an_instance_of Bike
-		expect(bike).to be_working
-
-	end
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+	  end
+  end
 
   it 'docks a bike' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(subject).to respond_to(:dock).with(1).argument
     subject.dock(bike)
     expect(subject.instance_variable_defined?(:@bike)).to eq true
     expect(subject.bike).to eq bike
   end
 
-  it 'cannot release when no bike' do
-    expect {subject.release_bike}.to raise_error
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 
 end
+
+

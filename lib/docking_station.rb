190b3b1f63b48b7008a72a1da0f70552121731f3
Rosie <r.gollancz@gmail.com>
2016-09-29 12:47:30 +0100

    Add bikes variable. Fix test for release bike method for updated bikes variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,22 @@
 require_relative 'bike'
 class DockingStation
 
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+    20.times { @bikes.push(Bike.new) }
+  end
 
   def release_bike
-  	raise 'No bikes available' unless @bike
-    @bike
+  	raise 'No bikes available' unless @bikes
+    @bike = @bikes.last
+    # @bikes.pop
   end
 
   def dock(bike)
-  	raise 'Docking station full' unless @bike == nil
-    @bike = bike
+  	raise 'Docking station full' unless @bikes == nil
+    @bikes = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,13 +5,13 @@ describe DockingStation do
   it {is_expected.to respond_to :release_bike}
 
   describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+    it 'releases a bike', focus: :true do
+      expect(subject.release_bike).to eq subject.bikes.last
 	  end
   end
+  # add test to check count of bikes goes down by 1 after release_bike is called
 
+  # ABOUT TO FIX THIS TEST FOR NEW VARIABLE NAMES
   it 'docks a bike' do
     bike = Bike.new
     expect(subject).to respond_to(:dock).with(1).argument
@@ -30,6 +30,10 @@ describe DockingStation do
       subject.dock(Bike.new)
       expect {subject.dock(Bike.new)}.to raise_error 'Docking station full'
     end
+  end
+
+  it 'starts with 20 bikes docked' do
+  expect(subject).to have_attributes(20.times { subject.dock Bike.new } )
   end
 
 end
